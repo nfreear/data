37a8cea6f9122a5d6c909274ef7eae8ea12f43f0
Nick Freear <nfreear@yahoo.co.uk>
2020-03-07 20:43:29 +0000

    Initial commit ~ Collated data, sample data, proverbs, quotes, English and Chinese.
    * https://github.com/nfreear/data.git

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author NDF, 07-Mar-2020.
+ */
+
+module.exports = {
+  bbcWeatherLocations: require('./data/bbc-weather-locations'),
+  chinaHighlightsChineseSayingsZh: require('./data/chinahighlights-chinese-sayings.zh-cn'),
+  gistSigned0QuotesEn: require('./data/gist-signed0-quotes.en'),
+  ltiChineseProverbsZh: require('./data/lti-chinese-proverbs.zh-cn'),
+};
