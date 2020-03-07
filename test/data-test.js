c98cb478c22a158acad81f11a36a4fe77452d20d
Nick Freear <nfreear@yahoo.co.uk>
2020-03-07 22:42:47 +0000

    Run `npm test` in a GitHub workflow - parse data with `jsonlint`

---
+++ test/data-test.js
@@ -0,0 +1,27 @@
+/**
+ * Lint each JSON data-file with `jsonlint`.
+ */
+
+const PKG = require('../package');
+const jsonlint = require("jsonlint");
+const fs = require('fs');
+const path = require('path');
+// const INDEX = require('../index');
+
+const JSON_FILES = PKG.files.filter(file => /\.json$/.test(file));
+
+JSON_FILES.forEach(async (jsonFile, idx) => {
+  try {
+    const PATH = path.join(__dirname, '..', jsonFile);
+    const JSON = await fs.promises.readFile(PATH, 'utf8');
+
+    const DATA = jsonlint.parse(JSON);
+
+    console.log(idx + 1, 'File:', jsonFile, '- count:', DATA.data.length);
+  } catch (ex) {
+    console.error(`>> Error in JSON file ${idx + 1}: '${jsonFile}'`);
+    console.error(ex.message);
+
+    process.exit( 1 );
+  }
+});
