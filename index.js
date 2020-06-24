73b292fdeb4bed9f082a4ddc98cbe38fac813eef
Nick Freear <nfreear@yahoo.co.uk>
2020-06-24 11:15:13 +0100

    Bug #2, Add 3rd BBC sounds JSON file; Add more Chinese sayings

--- index.js
+++ index.js
@@ -6,8 +6,11 @@
 module.exports = {
   BBC_NEWS_FEEDS_EN: require('./data/bbc-news-feeds.en'),
   // https://rms.api.bbc.co.uk/v2/my/programmes/favourites/playable?offset=0&limit=100;
-  BBC_SOUNDS_BOOKMARKS_I : require('./data/bbc-sounds-bookmarks-offset-0-limit-100'),
-  BBC_SOUNDS_BOOKMARKS_II: require('./data/bbc-sounds-bookmarks-offset-99-limit-100'),
+  BBC_SOUNDS_BOOKMARKS: [
+    require('./data/bbc-sounds-bookmarks-offset-0-limit-100'),
+    require('./data/bbc-sounds-bookmarks-offset-99-limit-100'),
+    require('./data/bbc-sounds-bookmarks-offset-199-limit-100'),
+  ],
   BBC_WEATHER_LOCATIONS:  require('./data/bbc-weather-locations'),
   PREBID_CURRENCY: require('./data/prebid-currency-file-latest'),
   PROVERBS_HIGHLIGHTS_ZH: require('./data/chinahighlights-chinese-sayings.zh-cn'),
